5412e313c8e76e31c97f674fb501639e95f8ec07
81ch <yaich1.8ichi@gmail.com>
2017-07-21 11:48:04 +0000

    kadai-tasklist_take4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
     
     if @task.destroy
       flash[:success] = 'タスクは正常に削除されました'
-      redirect_to task_url
+      redirect_to tasks_url
     end
     
   end
